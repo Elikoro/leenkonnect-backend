a09bbd51cbf00d4d26c30c7bdedbab321112426f
leeirabor <elishairabor@gmail.com>
2025-12-12 18:21:36 +0100

    backup before EB recreate

--- core/settings.py
+++ core/settings.py
@@ -153,6 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# Directory where `collectstatic` will gather static files for production
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
